8f67649d75ed169df2d09723830057abcb6c3a15
laungcisin <laungcisin@qq.com>
2018-01-22 10:52:16 +0800

    更改注释

--- laungcisin-security-core/src/main/java/com/laungcisin/security/core/authentication/AbstractChannelSecurityConfig.java
+++ laungcisin-security-core/src/main/java/com/laungcisin/security/core/authentication/AbstractChannelSecurityConfig.java
@@ -20,13 +20,14 @@ public class AbstractChannelSecurityConfig extends WebSecurityConfigurerAdapter 
 
     protected void applyPasswordAuthenticationConfig(HttpSecurity http) throws Exception {
         http.formLogin()//使用表单登录，不再使用默认httpBasic方式
+                .loginPage(SecurityConstants.DEFAULT_LOGIN_PAGE_URL)// 指定登录页面
 
                 /*
-                如果请求的URL需要认证则跳转的URL
-                SecurityConstants.DEFAULT_UNAUTHENTICATION_URL 此url的处理由 BrowserSecurityController.requireAuthentication方法处理
+                如果请求的URL需要认证，则跳转到loginProcessingUrl指定的url进行认证
+                    SecurityConstants.DEFAULT_UNAUTHENTICATION_URL定义的url，
+                    由 BrowserSecurityController.requireAuthentication方法处理
                  */
-                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)// 指定登录页面
-                .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)//处理表单中自定义的登录URL
+                .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)//需要认证时，跳转到此URL
                 .successHandler(laungcisinAuthenticationSuccessHandler)
                 .failureHandler(laungcisinAuthenticationFailureHandler)
                 .and().headers().frameOptions().disable()// 解决IFrame拒绝的问题
