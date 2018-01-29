45fce2beb7fdc81ff8a904dc7f57fe09eaa3ef57
laungcisin <laungcisin@qq.com>
2018-01-29 23:12:47 +0800

    修改schema.sql文件，增加注释

--- laungcisin-security-app/sql/schema.sql
+++ laungcisin-security-app/sql/schema.sql
@@ -14,6 +14,18 @@ create table oauth_client_details (
   autoapprove VARCHAR(256)
 );
 
+alter table oauth_client_details modify column client_id VARCHAR(256) comment '客户端ID';
+alter table oauth_client_details modify column resource_ids VARCHAR(256) comment '资源ID集合,多个资源时用逗号(,)分隔';
+alter table oauth_client_details modify column client_secret VARCHAR(256) comment '客户端密匙';
+alter table oauth_client_details modify column scope VARCHAR(256) comment '客户端申请的权限范围';
+alter table oauth_client_details modify column authorized_grant_types VARCHAR(256) comment '客户端支持的grant_type';
+alter table oauth_client_details modify column web_server_redirect_uri VARCHAR(256) comment '重定向URI';
+alter table oauth_client_details modify column authorities VARCHAR(256) comment '客户端所拥有的Spring Security的权限值，多个用逗号(,)分隔';
+alter table oauth_client_details modify column access_token_validity INTEGER comment '访问令牌有效时间值(单位:秒)';
+alter table oauth_client_details modify column refresh_token_validity INTEGER comment '更新令牌有效时间值(单位:秒)';
+alter table oauth_client_details modify column additional_information VARCHAR(256) comment '预留字段';
+alter table oauth_client_details modify column autoapprove VARCHAR(256) comment '用户是否自动Approval操作';
+
 -- ----------------------------
 -- Records of oauth_client_details - test data
 -- ----------------------------
@@ -29,6 +41,12 @@ create table oauth_client_token (
   client_id VARCHAR(256)
 );
 
+alter table oauth_client_token modify column token_id VARCHAR(256) comment 'MD5加密的access_token值';
+alter table oauth_client_token modify column token BLOB comment 'OAuth2AccessToken.java对象序列化后的二进制数据';
+alter table oauth_client_token modify column authentication_id VARCHAR(256) comment 'MD5加密过的username,client_id,scope';
+alter table oauth_client_token modify column user_name VARCHAR(256) comment '登录的用户名';
+alter table oauth_client_token modify column client_id VARCHAR(256) comment '客户端ID';
+
 DROP TABLE IF EXISTS `oauth_access_token`;
 create table oauth_access_token (
   token_id VARCHAR(256),
@@ -40,6 +58,15 @@ create table oauth_access_token (
   refresh_token VARCHAR(256)
 );
 
+alter table oauth_access_token modify column token_id VARCHAR(256) comment 'MD5加密的access_token的值';
+alter table oauth_access_token modify column token BLOB comment 'OAuth2AccessToken.java对象序列化后的二进制数据';
+alter table oauth_access_token modify column authentication_id VARCHAR(256) comment 'MD5加密过的username,client_id,scope';
+alter table oauth_access_token modify column user_name VARCHAR(256) comment '登录的用户名';
+alter table oauth_access_token modify column client_id VARCHAR(256) comment '客户端ID';
+alter table oauth_access_token modify column authentication BLOB comment 'OAuth2Authentication.java对象序列化后的二进制数据';
+alter table oauth_access_token modify column refresh_token VARCHAR(256) comment 'MD5加密果的refresh_token的值';
+
+
 DROP TABLE IF EXISTS `oauth_refresh_token`;
 create table oauth_refresh_token (
   token_id VARCHAR(256),
@@ -47,20 +74,35 @@ create table oauth_refresh_token (
   authentication BLOB
 );
 
+alter table oauth_refresh_token modify column token_id VARCHAR(256) comment 'MD5加密过的refresh_token的值';
+alter table oauth_refresh_token modify column token BLOB comment 'OAuth2RefreshToken.java对象序列化后的二进制数据';
+alter table oauth_refresh_token modify column authentication VARCHAR(256) comment 'OAuth2Authentication.java对象序列化后的二进制数据';
+
 DROP TABLE IF EXISTS `oauth_code`;
 create table oauth_code (
-  code VARCHAR(256), authentication BLOB
+  code VARCHAR(256),
+  authentication BLOB
 );
+
+alter table oauth_code modify column code VARCHAR(256) comment '授权码(未加密)';
+alter table oauth_code modify column authentication BLOB comment 'AuthorizationRequestHolder.java对象序列化后的二进制数据';
 
 DROP TABLE IF EXISTS `oauth_approvals`;
 create table oauth_approvals (
-	userId VARCHAR(256),
-	clientId VARCHAR(256),
-	scope VARCHAR(256),
-	status VARCHAR(10),
-	expiresAt TIMESTAMP,
-	lastModifiedAt TIMESTAMP
+  userId VARCHAR(256),
+  clientId VARCHAR(256),
+  scope VARCHAR(256),
+  status VARCHAR(10),
+  expiresAt TIMESTAMP,
+  lastModifiedAt TIMESTAMP
 );
+
+alter table oauth_approvals modify column userid VARCHAR(256) comment '登录的用户名';
+alter table oauth_approvals modify column clientid VARCHAR(256) comment '客户端ID';
+alter table oauth_approvals modify column scope VARCHAR(256) comment '申请的权限';
+alter table oauth_approvals modify column status VARCHAR(256) comment '状态（Approve或Deny）';
+alter table oauth_approvals modify column expiresat TIMESTAMP comment '过期时间';
+alter table oauth_approvals modify column lastmodifiedat TIMESTAMP comment '最终修改时间';
 
 
 -- customized oauth_client_details table
